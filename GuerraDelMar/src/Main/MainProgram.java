2be310b259439b0b3b0cc4f9e74fb88a3fbe490c
nlrarojas <nlrarojas@hotmail.com>
2017-06-10 22:41:40 -0600

    Estructura básica del proyecto

---
+++ GuerraDelMar/src/Main/MainProgram.java
@@ -0,0 +1,14 @@
+package Main;
+
+import view.PrincipalWindow;
+
+/**
+ *
+ * @author Nelson
+ */
+public class MainProgram {
+
+    public static void main(String[] args) {         
+        new PrincipalWindow();
+    }
+}
